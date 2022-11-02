8be0660acea34aec406d72b3ea075c641ac6ff6b
Liora_Chemla <liora.chemla@efrei.net>
2022-11-02 16:20:49 +0100

    code with current date

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from datetime import date
+from pydantic import BaseModel
+
+class P(BaseModel):
+    message: str
+    currentdate: str
 
 app = FastAPI()
 
 @app.get("/multiply")
 
+
 async def root(number : int = 1):
 
     return {"result": number*2}
+
+@app.get("/hello")
+async def root2():
+    d = date.today()
+    return {"message": "Hello you", "current date": d}
+
+
